d6637efa6900fe0caa0e2b6963a8d31669d24608
Suryansh2204 <suryanshsrivastava22@gmail.com>
2022-09-10 18:11:35 +0530

    card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 export default function Card(){
     return(
-        <></>
+        <Wrapper>
+            <Image></Image>
+            <Status></Status>
+            <Text></Text>
+        </Wrapper>
     );
 }
+
+const Wrapper=styled.div``;
+
+const Image=styled.img``;
+
+const Status=styled.div``;
+
+const TText=styled.div``;
